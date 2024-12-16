5b6ef15010334c7e66b560bdfbe84cf32b16d528
s-jaswal <525SJASWAL@FRHSD.COM>
2024-12-16 10:25:47 -0500

    Class & Style Binding - done

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inStock: 10,  // Update this to a number for inventory tracking
             details: ['50% cotton', '30% wool', '20% polyester'],
+            activeClass: true,
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
