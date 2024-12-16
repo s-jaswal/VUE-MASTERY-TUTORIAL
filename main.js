ef9f745281ca39596ad644bd4f743811d7e1bc04
s-jaswal <525SJASWAL@FRHSD.COM>
2024-12-16 10:40:43 -0500

    Done with Computed Properties

--- main.js
+++ main.js
@@ -3,22 +3,32 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inStock: 10,  // Update this to a number for inventory tracking
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            activeClass: true,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0;
         }
     }
 });
