78289f4ad6177970e52b388ec1e8f76d9b50d700
s-jaswal <525SJASWAL@FRHSD.COM>
2024-12-16 09:57:40 -0500

    List Rendering - done

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inStock: true
-        }
+            inventory: 20,
+            details: ['50% cotton', '30% wool', '20% polyester']
+        };
     }
-})
+});
+
+// Mount the Vue app
+app.mount('#app');
